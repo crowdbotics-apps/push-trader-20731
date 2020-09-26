f942d147e3cd4d6a1100fb973485ecd514d66549
Crowdbotics <team@crowdbotics.com>
2020-09-26 15:43:31 +0000

    Update models

--- backend/push_trader_20731/wsgi.py
+++ backend/push_trader_20731/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'push_trader_20731.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "push_trader_20731.settings")
 
 application = get_wsgi_application()
